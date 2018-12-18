3a9d9eecebdd4f080f373297c2a8a8e1f5b6892b
liusong <song.liu@mytogo.com>
2018-12-18 15:56:35 +0800

    no message

---
+++ leetcode/leetcode/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  leetcode
+//
+//  Created by liusong on 2018/12/18.
+//  Copyright © 2018年 liusong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+
+    return 0;
+}
