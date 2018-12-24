a287d5067c5ef635b7fa89a9709cc0c719ec12e7
liusong <song.liu@mytogo.com>
2018-12-24 22:21:06 +0800

    no message

--- leetcode/leetcode/main-14.reverseListBetween.mm
+++ leetcode/leetcode/main-14.reverseListBetween.mm
@@ -20,33 +20,49 @@ struct ListNode
      ListNode(int x) : val(x), next(NULL) {}
 };
 
+/*  1->2->3->4->5->6->7
+ 先反转中间部分 m=3,n=5
+    1->2  3<-4<-5  6->7
+ 
+ 保存1 和5节点
+ 
+ 将2->next=5  3->next=6
+ 
+ */
 class Solution
 {
 public:
-    ListNode* reverseBetween(struct ListNode* head, int m, int n) {
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
         if(head == NULL || n - m <= 0)
             return head;
-        struct ListNode* cur = head;
-        struct ListNode* end = cur;
-        struct ListNode* prev = NULL;
-        struct ListNode* next = NULL;
-        struct ListNode* start = cur;
+        
+        //第一轮for使用
+        ListNode* cur = head;
+        ListNode* start = cur;//保存2号结点
+        ListNode* end = cur;//保存3号结点
+        
         int i = 1;
-        for(; i < m ; ++i)
-        {
-            start = cur;//保存1号结点
+        for(; i < m ; i++){
+            start = cur;
             cur = cur->next;
-            end = cur;//保存2号结点
+            end = cur;
         }
-        for(i = 0; i <= n - m ; ++i)
-        {
+        //此时cur->3  start->2 end->3
+        
+        //第二轮for使用 交换节点
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        for(i = 0; i <= n - m ; i++){
             next = cur->next;
             cur->next = prev;
             prev = cur;
             cur = next;
-        }//实现2->4 3个结点的逆置，此时2的next指向空，cur和next都指向了 5，prev指向的是 4
-        start->next = prev;//1 的next指向 4
-        end->next = cur;//2 的next指向 5
+        }
+        //此时next->6 start->2 end->3  实现了 3<-4<-5  prev->5  cur->6
+        
+        start->next = prev;//2 的next指向 5
+        end->next = cur;//3 的next指向 6
         if(m == 1)
             return prev;//如果头结点也参与了逆置，那么新的头结点是逆置链表中的最后一个结点，也就是 4 结点
         return head;
@@ -71,12 +87,16 @@ int main(int argc, const char * argv[]) {
     ListNode *node3=new ListNode(3);
     ListNode *node4=new ListNode(4);
     ListNode *node5=new ListNode(5);
+    ListNode *node6=new ListNode(6);
+    ListNode *node7=new ListNode(7);
     node1->next=node2;
     node2->next=node3;
     node3->next=node4;
     node4->next=node5;
+    node5->next=node6;
+    node6->next=node7;
     Solution so=Solution();
-    ListNode *newHead=so.reverseBetween(node1, 2, 4);
+    ListNode *newHead=so.reverseBetween(node1, 3, 5);
     printNode(newHead);
 
     return 0;
