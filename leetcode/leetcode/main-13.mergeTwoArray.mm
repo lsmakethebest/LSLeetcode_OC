e0fda4e7cdb23362a7e63d61d32a88cd088743cd
liusong <song.liu@mytogo.com>
2018-12-24 21:48:32 +0800

    no message

--- leetcode/leetcode/main-13.mergeTwoArray.mm
+++ leetcode/leetcode/main-13.mergeTwoArray.mm
@@ -56,27 +56,43 @@ using namespace std;
 //    }
 //};
 
+
+
+/* 当两个nums地址相离很近的时候，容易导致地址产生碰撞，导致对nums[k-1]赋值的时候更改了nums2的值，
+   导致结果不一致
+ 
+ num1下标地址
+0x100414650  1
+0x100414654  2
+0x100414658  3
+ 
+ num2下标地址
+0x100414660  1
+0x100414664  5
+0x100414668  6
+*/
+ 
 #pragma mark - 第三种解法，不新建数组，直接操作nums1
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int k=m+n;
         while(m>0 && n>0){
-            if(nums1[m-1] >= nums2[n-1]){
+            if(nums1[m-1]>=nums2[n-1]){
                 nums1[k-1] = nums1[m-1];
-                --k;
-                --m;
+                k--;
+                m--;
             }
             else{
                 nums1[k-1] = nums2[n-1];
-                --k;
-                --n;
+                k--;
+                n--;
             }
         }
         while(n > 0){
             nums1[k-1] = nums2[n-1];
-            --k;
-            --n;
+            k--;
+            n--;
         }
     }
 };
@@ -96,19 +112,24 @@ int main()
     nums1.push_back(1);
     nums1.push_back(2);
     nums1.push_back(3);
+    //必须push0，否则容易导致地址碰撞，导致结果和预期结果不一致
+    nums1.push_back(0);
+    nums1.push_back(0);
+    nums1.push_back(0);
+    
     
     vector<int> nums2;
-    nums2.push_back(2);
+    nums2.push_back(1);
     nums2.push_back(5);
     nums2.push_back(6);
     so.merge(nums1, 3, nums2, 3);
     
-    //第一种解法打印 不能使用 nums1.size() ，因为nums1.size()并没有改变
+    //第一,三种解法打印 不能使用 nums1.size() ，因为nums1.size()并没有改变
     for (int i=0; i<6; i++) {
         cout<<nums1[i]<<endl;
     }
     
-    //第二,三种解法可以这样打印使用 nums1.size() 个数
+    //第二种解法可以这样打印使用 nums1.size() 个数
 //    for (int i=0; i<nums1.size(); i++) {
 //        cout<<nums1[i]<<endl;
 //    }
